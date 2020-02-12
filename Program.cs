ab3c5fd8735cb052085afaad1b0ea34464e604a9
Bulat Shakurov <b1.shakurov@gmail.com>
2020-02-12 12:15:37 +0300

    add Delegate, method ClassStringTes

--- Program.cs
+++ Program.cs
@@ -7,14 +7,29 @@ using System.Threading.Tasks;
 namespace GitTestOne
 {
     class Program
+
     {
+
+        delegate void delgate(string m);
         static void Main(string[] args)
         {
+            delgate d1 = ClassStringTest;
+            d1?.Invoke(null);
+
 
 
 
 
             Console.ReadKey();
+        }
+
+
+        public static void ClassStringTest(string ms)
+        {
+            string mess1= ms;
+
+            string sForm = String.Format($"String.Format is :{mess1}, Data now: {DateTime.Now}");
+            Console.WriteLine(sForm);
         }
     }
 }
